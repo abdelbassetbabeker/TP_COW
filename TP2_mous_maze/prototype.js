0af2918491bc3ffaeba6413f0f500de002993bf4
abdelbassetbabeker <abdelbasset.babeker@univ-constantine2.dz>
2022-10-15 10:59:36 -0400

    maze game adding Start Game and Cant cheat handelers

--- TP2_mous_maze/prototype.js
+++ TP2_mous_maze/prototype.js
@@ -3,34 +3,71 @@
 
 let walls = document.querySelectorAll("div.boundary");
 let end = document.getElementById("end");
+let start = document.getElementById("start");
+let maze = document.getElementById("maze");
 
 
 
-end.addEventListener('mouseover', () => {
-    var testlose = walls[0].classList
-    console.log(testlose[1])
 
-    if (testlose[1] === 'youlose') {
-        alert('You lose the game , Try again')
+function youlose() {
+    walls[0].classList.add('youlose')
+    walls[1].classList.add('youlose')
+    walls[2].classList.add('youlose')
+    walls[3].classList.add('youlose')
+    walls[4].classList.add('youlose')
+}
+
+
+
+start.addEventListener('click', () => (
+    handlerStart()
+)
+)
+
+
+
+
+
+//code
+
+
+
+function handlerStart() {
+
+
+    maze.addEventListener('mouseleave', e => {
+        youlose()
     }
-    else {
-        alert('You win exelent ')
+    )
 
+    for (let i = 0; i <= walls.length; i++) {
+        start.addEventListener('click', () => {
+            walls[i].classList.remove('youlose')
+        })
     }
-});
 
-for (let i = 0; i <= walls.length; i++) {
-    walls[i].addEventListener('mouseover', () => {
-        walls[0].classList.add('youlose')
-        walls[1].classList.add('youlose')
-        walls[2].classList.add('youlose')
-        walls[3].classList.add('youlose')
-        walls[4].classList.add('youlose')
+    end.addEventListener('mouseover', () => {
+        var testlose = walls[0].classList
 
+        if (testlose[1] === 'youlose') {
+            alert('You lose the game , Try again')
+        }
+        else {
+            alert('You win exelent ')
+
+
+        }
+        window.location.reload(true)
 
     })
+    for (let i = 0; i <= walls.length; i++) {
+        walls[i].addEventListener('mouseover', youlose)
+    };
 
-};
+
+}
+
+
 
 
 
